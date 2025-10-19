a4bfadb82f945a563204acb564c9c93f5fe35aea
Keitumetse Setati <601428@student.belgiumcampus.ac.za>
2025-10-19 20:13:23 +0200

    Added the frontend and the backend of the profile

---
+++ CampusLearnPlatform/CampusLearnPlatform/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace CampusLearnPlatform.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+
+        // NEW
+        public string? ProfileInfo { get; set; }
+        public string? ProfilePictureUrl { get; set; } // e.g. "/uploads/profiles/abc.jpg"
+    }
+}
---
+++ CampusLearnPlatform/CampusLearnPlatform/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CampusLearnPlatform.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+
+        public string? ProfileInfo { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+
+        public bool IsMe { get; set; }
+
+        // For uploads
+        public IFormFile? ProfileImage { get; set; }
+    }
+}
