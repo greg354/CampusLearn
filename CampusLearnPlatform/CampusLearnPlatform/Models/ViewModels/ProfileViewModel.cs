6113287168aa4091dca16d1d4ff0ed24f6865f9f
Keitumetse Setati <601428@student.belgiumcampus.ac.za>
2025-10-20 02:06:43 +0200

    Added fuctionality to the profile view

--- CampusLearnPlatform/CampusLearnPlatform/Models/ViewModels/ProfileViewModel.cs
+++ CampusLearnPlatform/CampusLearnPlatform/Models/ViewModels/ProfileViewModel.cs
@@ -3,16 +3,13 @@
     public class ProfileViewModel
     {
         public Guid Id { get; set; }
-
         public string Name { get; set; } = "";
         public string Email { get; set; } = "";
-
         public string? ProfileInfo { get; set; }
         public string? ProfilePictureUrl { get; set; }
-
         public bool IsMe { get; set; }
 
-        // For uploads
+        // NEW: file from file explorer
         public IFormFile? ProfileImage { get; set; }
     }
 }
